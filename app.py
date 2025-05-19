da72031f6eca49890b8b281dde290f77d1f43645
Adel Muhammed <adelmuhammed786@gmail.com>
2025-05-19 11:45:39 +0530

    Added Flask app, templates, and static files

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, send_file, render_template
+import yt_dlp
+import os
+import uuid
+
+app = Flask(__name__)
+
+DOWNLOAD_FOLDER = "downloads"
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    format = request.form['format']  # 'mp3' or 'mp4'
+    filename = str(uuid.uuid4())
+    filepath = os.path.join(DOWNLOAD_FOLDER, filename)
+
+    ydl_opts = {
+        'outtmpl': filepath + '.%(ext)s',
+    }
+
+    if format == 'mp3':
+        ydl_opts.update({
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        })
+    else:  # MP4
+        ydl_opts.update({
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+            'merge_output_format': 'mp4',
+        })
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.download([url])
+
+    ext = 'mp3' if format == 'mp3' else 'mp4'
+    return send_file(filepath + '.' + ext, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
